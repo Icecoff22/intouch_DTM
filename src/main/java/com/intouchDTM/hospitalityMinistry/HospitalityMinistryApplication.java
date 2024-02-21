9d4ff19aecfad6eb66a7d903a932f6e3b32a6067
icecoff22 <icecoff2222@gmail.com>
2024-02-21 11:11:11 +0900

    init

---
+++ src/main/java/com/intouchDTM/hospitalityMinistry/HospitalityMinistryApplication.java
@@ -0,0 +1,13 @@
+package com.intouchDTM.hospitalityMinistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalityMinistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalityMinistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intouchDTM/hospitalityMinistry/HospitalityMinistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intouchDTM.hospitalityMinistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalityMinistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
